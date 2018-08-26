711a10f8725bcebbf6e4f530d121cd054c558ab8
Chad Kruszynsky <ckruszynsky@gmail.com>
2018-08-26 12:20:35 -0500

    Added console.clear to AsComponent

--- src/common/as-component.js
+++ src/common/as-component.js
@@ -3,6 +3,7 @@ import React from "react";
 export function asComponent(fn) {
   return class ExampleComponent extends React.Component {
     render() {
+      console.clear();
       return fn() || null;
     }
   };
